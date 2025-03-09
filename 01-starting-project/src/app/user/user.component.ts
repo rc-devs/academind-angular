bd484fce6ec160dc6c77da92316acf148c2fa299
rc-devs <rc.devs9@gmail.com>
2025-03-09 08:38:07 -0500

    back to Input decorator and zone stuff

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component,  Input, computed, input} from '@angular/core';
+import { Component,  Input} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,12 +8,12 @@ import { Component,  Input, computed, input} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  avatar = input.required<string>();
-  name = input.required<string>();
+ @Input ({required: true}) avatar!: string;
+ @Input ({required: true}) name!: string;
 
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  });
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  };
 
 
   onSelectUser() {
