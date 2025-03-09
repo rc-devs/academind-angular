7f2173b061e72bcee2ee4abfe769bd420309d609
rc-devs <rc.devs9@gmail.com>
2025-03-09 07:49:39 -0500

    remove signals and computed from user.component.ts

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,8 +1,6 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component,  } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -12,15 +10,7 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
- /*  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar
-  } */
-
   onSelectUser() {
     console.log('clicked!');
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser.set(DUMMY_USERS[randomIndex])
   }
 }
