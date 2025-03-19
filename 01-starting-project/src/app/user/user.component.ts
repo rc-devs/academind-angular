de8a87b2db59f2844ee2761ec8a268fe579b5423
rc-devs <rc.devs9@gmail.com>
2025-03-19 11:44:47 -0500

    signals

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
-import { Component,  Input} from '@angular/core';
+import { Component, input, computed, signal} from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -8,15 +11,22 @@ import { Component,  Input} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
- @Input ({required: true}) avatar!: string;
- @Input ({required: true}) name!: string;
+// @Input({ required: true }) avatar!: string
+// @Input({ required: true }) name!: string
+  avatar = input.required<string>();
+  name = input.required<string>();
 
-  get imagePath() {
+
+ selectedUser = signal(DUMMY_USERS[randomIndex]);
+ imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+
+/*   get imagePath() {
     return 'assets/users/' + this.avatar;
   };
-
+ */
 
   onSelectUser() {
-    console.log('clicked!');
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
