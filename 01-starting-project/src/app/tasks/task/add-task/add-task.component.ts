fbbc9f8b89c24c35309431e89bc9beb49c5bd8c6
rc-devs <rc.devs9@gmail.com>
2025-03-19 14:43:12 -0500

    two way bind enteredTitle, date, summary for add task card

--- 01-starting-project/src/app/tasks/task/add-task/add-task.component.ts
+++ 01-starting-project/src/app/tasks/task/add-task/add-task.component.ts
@@ -1,14 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule, } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent {
  @Output() cancel = new EventEmitter<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
  onCancel(){
   this.cancel.emit();
