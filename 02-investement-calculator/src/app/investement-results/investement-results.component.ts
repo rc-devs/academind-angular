d99a70c31b39726b27ac90e4988f36ee0b799e30
rc-devs <rc.devs9@gmail.com>
2025-03-22 13:06:15 -0500

    create investment results component, input css

---
+++ 02-investement-calculator/src/app/investement-results/investement-results.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investement-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './investement-results.component.html',
+  styleUrl: './investement-results.component.css'
+})
+export class InvestementResultsComponent {
+
+}
