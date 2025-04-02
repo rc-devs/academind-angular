4ca1225cb591a3b5a030f7e6f5cbb2d4ea12ddf5
rc-devs <rc.devs9@gmail.com>
2025-04-02 11:56:46 -0500

    input data for display via dashboard item, connect dashboard-item to app

--- 03-components-deep-dive/src/app/app.component.ts
+++ 03-components-deep-dive/src/app/app.component.ts
@@ -3,10 +3,18 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TrafficComponent } from './dashboard/traffic/traffic.component';
 import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
+  imports:
+  [
+    HeaderComponent,
+     ServerStatusComponent,
+     TrafficComponent,
+     TicketsComponent,
+     DashboardItemComponent
+  ],
   standalone: true,
   templateUrl: './app.component.html',
 })
