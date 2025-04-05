b6469431f1631ab39c574b290abf216ada6d97ac
rc-devs <rc.devs9@gmail.com>
2025-04-05 17:42:14 -0500

    elementref option

--- 03-components-deep-dive/src/app/shared/control/control.component.ts
+++ 03-components-deep-dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, input, ViewEncapsulation,} from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation,} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,11 +8,20 @@ import { Component, HostBinding, input, ViewEncapsulation,} from '@angular/core'
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None, //sets styles in component to global (none disables it)
   host: {
-    class: 'control'
-  } //add key-value pairs as properties to app-control wherever it is being used
+    class: 'control', //adds to component as attribute
+    '(click)': 'onClick()', //adds event listener
+  } //add key-value pairs as properties to host element(app-control in this case) wherever it is being used (for css?)
 })
 export class ControlComponent {
   //@HostBinding('class') className = "control"; an alternative way to bind via class
+  //@HostListener() //for event binding, assign method and event to be listened as param
   label = input.required<string>();
+  private el = inject(ElementRef);
+
+  onClick(){
+    console.log("Clicked")
+    console.log(this.el)
+  }
+
 
 }
