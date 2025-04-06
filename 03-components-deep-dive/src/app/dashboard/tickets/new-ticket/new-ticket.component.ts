63d15411eb3be82deb7e46cfbc325c58948ad258
rc-devs <rc.devs9@gmail.com>
2025-04-06 17:49:07 -0500

    reset form via viewchild (i hate it)

--- 03-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 03-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,11 +11,12 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
 
-  onSubmit(title: string, request: string, form:HTMLFormElement){
+  onSubmit(title: string, request: string, ){
 
     console.log(title)
     console.log(request)
-    form.reset();
+    this.form?.nativeElement.reset();
   }
 }
