f01483e692501af63e59f27397a018c4e2ec0d31
rc-devs <rc.devs9@gmail.com>
2025-04-13 17:31:44 -0500

    request dependency, replace empty array with dependency data

--- 05-services-dependency-injection/src/app/tasks/tasks-list/tasks-list.component.ts
+++ 05-services-dependency-injection/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-tasks-list',
@@ -10,8 +11,9 @@ import { TaskItemComponent } from './task-item/task-item.component';
   imports: [TaskItemComponent],
 })
 export class TasksListComponent {
+  private tasksService = inject(TasksService); //request dependency from angular
   selectedFilter = signal<string>('all');
-  tasks = [];
+  tasks = this.tasksService.tasks; //replace empty array with the default from TaskService (this.tasksService) tasks property (.tasks)
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
