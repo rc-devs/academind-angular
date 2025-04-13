24e0f1fdc10b1cdaa99bde008e162a42fe31bca3
rc-devs <rc.devs9@gmail.com>
2025-04-13 18:21:57 -0500

    create logging service, utilize within task service

--- 05-services-dependency-injection/src/app/tasks/tasks.service.ts
+++ 05-services-dependency-injection/src/app/tasks/tasks.service.ts
@@ -1,11 +1,13 @@
-import { Injectable, signal } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
 import { Task, TaskStatus } from "./task.model";
+import { LoggingService } from "../logging.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
   private tasks = signal<Task[]>([]); //make data private (this component only)
+  private loggingService = inject(LoggingService);
 
   allTasks = this.tasks.asReadonly(); //tasks available publicly as readonly
 
@@ -15,10 +17,12 @@ export class TasksService {
       id: Math.random().toString(),
       status: 'OPEN'
     }
-    this.tasks.update((oldTasks) => [...oldTasks, newTask])
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+    this.loggingService.log('ADDED TASK WITH TITLE ' + taskData.title);
   }
 
   updateTaskStatus(taskId: string, newStatus: TaskStatus){
     this.tasks.update((oldTasks) => oldTasks.map((task) => task.id === taskId ? {...task, status: newStatus} : task)) //update task array by replacing the oldTask with the new task if the id matches, replacing the status of the task with the newStatus
+    this.loggingService.log('CHANGED TASK STATUS TO ' + newStatus);
   }
 }
